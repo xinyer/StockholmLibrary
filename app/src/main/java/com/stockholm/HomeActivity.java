25ab801977f4e373854a5c8582c5d35d9075bd09
wangxin <xinyer0510@gmail.com>
2016-12-20 17:35:06 +0800

    config jitpack

--- app/src/main/java/com/stockholm/HomeActivity.java
+++ app/src/main/java/com/stockholm/HomeActivity.java
@@ -4,13 +4,11 @@ package com.stockholm;
 import android.app.Activity;
 import android.os.Bundle;
 
-import com.stockholm.api.LoginService;
-
 public class HomeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        LoginService
+
     }
 }
