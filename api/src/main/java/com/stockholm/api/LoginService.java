f3e8bf5b03a11b874b4aeea9b868aae6a5205c1c
wangxin <xinyer0510@gmail.com>
2016-12-20 17:22:25 +0800

    init project

---
+++ api/src/main/java/com/stockholm/api/LoginService.java
@@ -0,0 +1,10 @@
+package com.stockholm.api;
+
+
+public class LoginService {
+
+    public void test() {
+        System.out.println("Hello login.");
+    }
+
+}
---
+++ app/src/main/java/com/stockholm/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.stockholm;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.stockholm.api.LoginService;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LoginService
+    }
+}
